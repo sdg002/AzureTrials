1f198d2b4536bbc7462bf41ea687adaf8c4ad2a7
saurabh <saurabh_dasgupta@hotmail.com>
2021-03-30 00:11:07 +0100

    Added doc links w.r.t APIM and PowerShell

--- apim/main.ps1
+++ apim/main.ps1
@@ -1,7 +1,27 @@
-#
-#The main script for Point2Point infra creation
-#https://docs.microsoft.com/en-us/azure/api-management/powershell-create-service-instance
-#
+<#
+The main script for Point2Point infra creation
+
+Important links
+---------------
+
+How to create a new APIM?
+-------------------------
+https://docs.microsoft.com/en-us/azure/api-management/powershell-create-service-instance
+
+
+Understanding the terminology of APIM
+---------------------------------------
+https://docs.microsoft.com/en-us/azure/api-management/api-management-terminology#term-definitions
+
+Powershell reference for APIM
+------------------------------
+https://docs.microsoft.com/en-us/powershell/module/az.apimanagement/new-azapimanagementproduct?view=azps-5.7.0
+
+$apimContext = New-AzApiManagementContext -ResourceGroupName "Api-Default-WestUS" -ServiceName "contoso"
+New-AzApiManagementProduct -Context $apimContext -ProductId "0123456789" -Title "Starter" -Description "Starter Product" -LegalTerms "Free for all" -SubscriptionRequired $False -State "Published"
+
+#>
+
 Set-StrictMode -Version "2.0"
 cls
 $ErrorActionPreference="Stop"
