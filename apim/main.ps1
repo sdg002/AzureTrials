b200a2103fb399681d43a7e85836df52c2c62a3a
saurabh <saurabh_dasgupta@hotmail.com>
2021-03-30 00:57:30 +0100

    Created APIM product

--- apim/main.ps1
+++ apim/main.ps1
@@ -20,6 +20,11 @@ https://docs.microsoft.com/en-us/powershell/module/az.apimanagement/new-azapiman
 $apimContext = New-AzApiManagementContext -ResourceGroupName "Api-Default-WestUS" -ServiceName "contoso"
 New-AzApiManagementProduct -Context $apimContext -ProductId "0123456789" -Title "Starter" -Description "Starter Product" -LegalTerms "Free for all" -SubscriptionRequired $False -State "Published"
 
+
+Guided tutorial
+---------------
+https://www.cloudsma.com/2019/09/configure-azure-api-management-powershell/
+
 #>
 
 Set-StrictMode -Version "2.0"
@@ -29,6 +34,7 @@ $rgroup="rg-demo-apim"
 $location="uksouth"
 $apim="sauapim"
 $adminEmail="saurabh_dasgupta@hotmail.com"
+$apimProduct="Contoso"
 #######################################################################
 "Creating resource group $rgroup"
 New-AzResourceGroup -Name $rgroup -Location $location
@@ -38,4 +44,43 @@ New-AzResourceGroup -Name $rgroup -Location $location
 "Creating new APIM"
 New-AzApiManagement -Name $apim -ResourceGroupName $rgroup `
   -Location $location -Organization "Contoso" -AdminEmail $adminEmail
+
+"New APIM created $apim"
+
+#
+#
+
+$apimObject=Get-AzAPIManagement -ResourceGroupName $rgroup -Name $apim
+"Got context from APIM"
+$apiContext = New-AzApiManagementContext -ResourceGroupName $apimObject.resourcegroupname -ServiceName $apimObject.name
+
+"Creating new product $apimProduct"
+$newProduct = New-AzApiManagementProduct -Context $apiContext -ProductId "123" `
+-Title "Demo title" -Description "Demo API Product for Contoso" -State "Published" 
+"New product $apimProduct created"
+
+#add-AzApiManagementApiToProduct -context $apiContext -ProductId $newproduct.productid -apiid $newapi.id
+
+#
+#Get the function app url
+#
+#"Get function app using Get-AzFunctionApp"
+#"Get webapp url using $app.defaulthostname"
+ # Find-Module -Filter "az.functions"
+
+
+ <#
+ #Get URL of the Azure function app
+ $azwebapp = get-azwebapp -ResourceGroupName yourresourcegroup
+ $uri = "https://" + $azwebapp.defaulthostname
+
+ #New API
+ $newapi = New-AzApiManagementApi -context $apiContext -name "azure function" -ServiceUrl $uri -protocols @('http','https') -path "testapi"
+ New-AzApiManagementOperation -Context $apiContext -ApiId $newapi.apiid -OperationId "operation1" -Name "HttpEndPoint" -Method "POST" -UrlTemplate "/HttpEndPoint"
+
+ #Create API operation
+ New-AzApiManagementOperation -Context $apiContext -ApiId $newapi.apiid -OperationId "operation1" -Name "HttpEndPoint" -Method "POST" -UrlTemplate "/HttpEndPoint"
+
+ #>
+
 
