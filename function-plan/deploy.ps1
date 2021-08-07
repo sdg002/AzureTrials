72c46aa840dccc0b76bc981bee946158fee4d213
saurabh <saurabh_dasgupta@hotmail.com>
2021-08-07 16:59:28 +0100

    Passing storage account name as a parameer to ARM

--- function-plan/deploy.ps1
+++ function-plan/deploy.ps1
@@ -5,6 +5,7 @@ $ErrorActionPreference="Stop"
 $ResourceGroup="rg-demo-consumptionfunction-plan"
 $Location="uksouth"
 $PlanName="MyDemoPlan001"
+$StorageAccountName="{0}sto" -f $PlanName.ToLower()
 #########################################
 #
 Write-Host "Creating new resource group $ResourceGroup"
@@ -13,8 +14,9 @@ New-AzResourceGroup -Name $ResourceGroup -Force -Location $Location | Out-Null
 #Deploy ARM template
 #
 $armFilePath=Join-Path -Path $PSScriptRoot -ChildPath "armtemplate\ConsumptionPlan.json"
-Write-Host "Deploying ARM template file $armFilePath to $ResourceGroup"
+Write-Host "Deploying ARM template file $armFilePath to $ResourceGroup with storage account $StorageAccountName"
 $armParameters=@{}
 $armParameters.Add("appName",$PlanName)
+$armParameters.Add("appStorageAccountName",$StorageAccountName)
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile $armFilePath -TemplateParameterObject $armParameters
 Write-Host "Deploying ARM template file $armFilePath to $ResourceGroup complete"
