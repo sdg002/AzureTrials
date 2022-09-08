909347f5dd7cce1dd566bb1913cb7d555b77faa8
saurabh <saurabh_dasgupta@hotmail.com>
2022-09-08 18:49:43 +0100

    Completed People unit test for count of rows

---
+++ synapse-sqlviews-integrationtesting/source/synapse-tests/IntegrationTests/AzureHelper.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Identity;
+using Azure.ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public class AzureHelper
+    {
+        internal async Task<string> GetDatabaseAccessToken()
+        {
+            var dbScope = "https://database.windows.net";
+
+            var creds = new AzureCliCredential();
+            var armClient = new ArmClient(creds); //Pass defaultSubscriptionId via environment
+            var defSub = await armClient.GetDefaultSubscriptionAsync();
+
+            var scopes = new string[] { dbScope };
+            var tenantId = defSub.Data.TenantId.ToString();
+            Trace.WriteLine($"Tenant id is {tenantId}");
+
+            var ctx = new Azure.Core.TokenRequestContext(scopes: scopes, tenantId: tenantId);
+
+            var token = await creds.GetTokenAsync(requestContext: ctx);
+            return token.Token;
+        }
+
+        internal Task<string> GetServerlessEndPoint()
+        {
+            string server = "testsynapse-dev-ondemand.sql.azuresynapse.net";
+            return Task.FromResult(server);
+        }
+    }
+}
---
+++ synapse-sqlviews-integrationtesting/source/synapse-tests/IntegrationTests/PeopleTableTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class PeopleTableTests
+    {
+        [TestMethod]
+        public async Task Test_For_Count_Of_Rows()
+        {
+            //Arrange
+            var azureHelper = new AzureHelper();
+            string dbAccessToken = await azureHelper.GetDatabaseAccessToken(); //fill this
+            string serverlessEndPoint = await azureHelper.GetServerlessEndPoint();
+
+            //Act
+            DataSet ds = await DbHelper.QueryDataSet(serverlessEndPoint, dbAccessToken, "SELECT * FROM PEOPLE ORDER BY ID");
+
+            //Assert
+            Assert.AreEqual(4, ds.Tables[0].Rows.Count);
+
+            var peopleTable = ds.Tables[0];
+
+            Assert.AreEqual(100, peopleTable.Rows[0]["ID"]);
+            Assert.AreEqual(200, peopleTable.Rows[1]["ID"]);
+            Assert.AreEqual(300, peopleTable.Rows[2]["ID"]);
+            Assert.AreEqual(400, peopleTable.Rows[3]["ID"]);
+        }
+    }
+}
