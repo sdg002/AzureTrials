c3df3cc0222c9ef0afd1c8b1d10a35e05246a495
saurabh <saurabh_dasgupta@hotmail.com>
2022-09-20 22:30:49 +0100

    Re-installed opencensus, in venv

--- docker-python/docs/notes.md
+++ docker-python/docs/notes.md
@@ -25,3 +25,53 @@ pip freeze > requirements.txt
 
 ---
 
+# Build a Python Docker image
+
+## Online documentation
+https://hub.docker.com/_/python
+
+## Which image for 3.9 ?
+3.9
+---
+
+## Sample Docker file
+```dockerfile
+FROM python:3
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD [ "python", "./your-daemon-or-script.py" ]
+```
+
+
+# Docker commands
+
+
+## List all images
+```powershell
+docker images
+```
+
+What to expect:
+```
+REPOSITORY        TAG                  IMAGE ID       CREATED         SIZE
+pythondemo        latest               ccf9191a139a   6 minutes ago   182MB
+jenkins/jenkins   2.289.3-lts-centos   219e37fa7afb   13 months ago   875MB
+
+```
+
+## Delete an image 
+```powershell
+docker rmi <image tag>
+```
+
+## Create a image from a Docker file
+This assumes you are in the folder which has a `Dockerfile` already
+```powershell
+docker build .\ -t pythondemo
+```
--- docker-python/infra/deployimage.ps1
+++ docker-python/infra/deployimage.ps1
@@ -22,3 +22,49 @@ CreateResourceGroup
 CreateApplicationInsights
 $instruKey=GetInstrumentationKey
 Write-Host ("Instrumenttion key = {0}" -f $instruKey)
+
+
+<#
+you were here
+
+Step 1
+------
+Create a container registry
+    & az acr create --resource-group $Global:ResourceGroup --name $ContainerRegistry --sku Basic --admin-enabled true
+
+Step 2
+------
+
+Write a separate script which will upload an image to the acr
+    Name this script as UploadDockerImage.ps1
+    You will need to follow https://learn.microsoft.com/en-us/cli/azure/container?view=azure-cli-latest#az-container-create
+    You need to specify the registry server and password
+
+    az container create -g MyResourceGroup --name myapp --image myAcrRegistry.azurecr.io/myimage:latest --registry-password password
+
+Refer the files here C:\Users\saurabhd\MyTrials\AzureStuff\Docker
+
+Step 3
+------
+The container has to be started
+    See this https://learn.microsoft.com/en-us/cli/azure/container?view=azure-cli-latest#az-container-start
+    az container start --name
+                   --resource-group
+                   [--no-wait]
+    Test
+    ----
+        Run from CMD and you should see logging to Application Insights
+        Try logging some more stuff like environment all variables
+
+Step 4 (No - do not go here.. Becomes too complex. Run the Python in a for loop, use a delay provided by the environment variables)
+------
+    Create an Azure batch account
+    See what you did in Azure batch
+    C:\Users\saurabhd\MyTrials\AzureStuff\batch-services
+    & az batch account create --name $BatchAccount --location $Global:Location --resource-group $Global:BatchServicesResourceGroup --storage-account $StorageAccountBachServices --identity-type "SystemAssigned"
+
+Step 5 (No - do not go here. See reasons above)
+------
+    Schedule the image to run every 1 minute
+
+#>
