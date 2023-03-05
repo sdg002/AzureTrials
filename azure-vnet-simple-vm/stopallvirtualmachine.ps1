4ab1d0294b815d604d229f88bd670dba37949b7e
saurabh <saurabh_dasgupta@hotmail.com>
2023-03-05 16:21:14 +0000

    Stop vm script

---
+++ azure-vnet-simple-vm/stopallvirtualmachine.ps1
@@ -0,0 +1,14 @@
+. $PSScriptRoot\common.ps1
+
+
+
+$listOfVms = (& az vm list --resource-group $Global:ResourceGroup) | ConvertFrom-Json -AsHashTable
+
+foreach($vm in $listOfVms)
+{
+    Write-Host ("Shutting down VM {0}" -f $vm["name"])
+    & az vm stop --resource-group $Global:ResourceGroup --name $vm["name"]
+    & az vm deallocate --resource-group $Global:ResourceGroup --name $vm["name"]
+}
+
+
