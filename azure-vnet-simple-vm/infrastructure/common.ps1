5e63db9a712c27d1df53abe92882fb2288118458
saurabh <saurabh_dasgupta@hotmail.com>
2023-03-12 01:19:46 +0000

    GetAcrPassword and GetAcrLogin

--- azure-vnet-simple-vm/infrastructure/common.ps1
+++ azure-vnet-simple-vm/infrastructure/common.ps1
@@ -12,3 +12,19 @@ function ThrowErrorIfExitCode($message){
     Write-Error -Message $message
 }
 
+
+function GetAcrLogin{
+    Write-Host "Begin. Getting all ACR details"
+    $acrInfo=az acr show --name $Global:ContainerRegistry --resource-group $Global:ResourceGroup | ConvertFrom-Json -AsHashtable
+    $acrLoginServer=$acrInfo["loginServer"]
+    Write-Host "Got the ACR login server: $acrLoginServer"
+    return $acrLoginServer
+}
+
+function GetAcrPassword{
+    Write-Host "Getting the ACR password"
+    $acrCredentials=(az acr credential show --name $Global:ContainerRegistry --resource-group $Global:ResourceGroup | ConvertFrom-Json -AsHashtable)
+    $password=$acrCredentials["passwords"][0]["value"]
+    Write-Host "Got the ACR password: $password"
+    return $password
+}
--- azure-vnet-simple-vm/infrastructure/deploy-container-instance.ps1
+++ azure-vnet-simple-vm/infrastructure/deploy-container-instance.ps1
@@ -1,16 +1,8 @@
 . $PSScriptRoot\common.ps1
 
 
-Write-Host "Getting the ACR password"
-$acrCredentials=(az acr credential show --name $Global:ContainerRegistry --resource-group $Global:ResourceGroup | ConvertFrom-Json -AsHashtable)
-$AcrPassword=$acrCredentials["passwords"][0]["value"]
-Write-Host "Got the ACR password: $AcrPassword"
-
-
-Write-Host "Begin. Getting all ACR details"
-$acrInfo=az acr show --name $Global:ContainerRegistry --resource-group $Global:ResourceGroup | ConvertFrom-Json -AsHashtable
-$AcrLoginServer=$acrInfo["loginServer"]
-Write-Host "Got the ACR login server: $AcrLoginServer"
+$AcrPassword=GetAcrPassword
+$AcrLoginServer=GetAcrLogin
 
 Write-Host "Begin-Creating Azure Container instance "
 $image="$AcrLoginServer/pythonworkerapp:latest"
