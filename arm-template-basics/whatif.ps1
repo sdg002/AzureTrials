7616d4084129eee5b4b4ac54884ccffbc211fb9f
saurabh <saurabh_dasgupta@hotmail.com>
2023-04-18 07:51:41 +0100

    string and int and array

--- arm-template-basics/whatif.ps1
+++ arm-template-basics/whatif.ps1
@@ -5,11 +5,13 @@ $ResourceGroup="rg-demo-vm-vnet-experiment"
 
 function ExecuteSimpleArmTemplateOutput($folder,$armFileName){
     $armFilePath = Join-path -Path $PSScriptRoot -ChildPath "$folder\$armFileName"
-    $outputFile=Join-path -Path "$PSScriptRoot\$subFolder" -ChildPath  "arm.output.json"
+    $outputFile=Join-path -Path "$PSScriptRoot\$folder" -ChildPath  "arm.output.json"
     az deployment group create --resource-group $ResourceGroup --template-file $armFilePath  --verbose > $outputFile
     Write-Host "The output was written to file file $outputFile"
 }
-ExecuteSimpleArmTemplateOutput -folder ".\1-arm-object-output" -armFileName "arm.json"
+
+#ExecuteSimpleArmTemplateOutput -folder ".\1-arm-object-output" -armFileName "arm.json"
+ExecuteSimpleArmTemplateOutput -folder ".\2-arm-string-int-output" -armFileName "arm.json"
 
 # $subFolder=".\1-arm-object-output"
 # $armFileName="$subFolder\arm.json"
