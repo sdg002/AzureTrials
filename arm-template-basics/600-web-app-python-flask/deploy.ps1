dc7a4e955e812058942c3c2e6b4d43ca69042dae
saurabh <saurabh_dasgupta@hotmail.com>
2023-05-12 20:40:52 +0100

    got some app service plan stuff  - skeletal

---
+++ arm-template-basics/600-web-app-python-flask/deploy.ps1
@@ -0,0 +1,11 @@
+. $PSScriptRoot/variables.ps1
+
+<#
+Script for deploying all Azure resources neccessary for a Python Flask Web apps
+#>
+
+Write-Host "Going to create the resource group: '$Global:ResourceGroup' with the location: '$Global:Location' "
+& az group create --location $Global:Location --name $Global:ResourceGroup `
+    --tags department=$Global:TagDepartment owner=$Global:TagOwner costcenter=$Global:TagCostCenter
+RaiseCliError -message "Failed to create the resource group $Global:ResourceGroup"
+
