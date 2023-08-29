444299d2e4cc042521808906be02aa64d644c50b
saurabh <saurabh_dasgupta@hotmail.com>
2023-08-29 18:28:31 +0100

    Got logging to work on App insights

--- azure-alert-msteams-webhook/csharp-webapp/csharp-webapp/Controllers/WeatherForecastController.cs
+++ azure-alert-msteams-webhook/csharp-webapp/csharp-webapp/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace csharp_webapp.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Inside method GetWeatherForecase (Information)");
+            _logger.LogWarning("Inside method GetWeatherForecase (Warning)");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
