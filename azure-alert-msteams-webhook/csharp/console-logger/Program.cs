8158a16c7d3c5b290396e328a909a51ca2862064
saurabh <saurabh_dasgupta@hotmail.com>
2023-09-05 18:14:06 +0100

    Changed logging to app insights

--- azure-alert-msteams-webhook/csharp/console-logger/Program.cs
+++ azure-alert-msteams-webhook/csharp/console-logger/Program.cs
@@ -1,27 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.WorkerService;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+//Refer https://learn.microsoft.com/en-us/azure/azure-monitor/app/ilogger?tabs=dotnet6#console-application
 Console.WriteLine("Hello, World!");
+using var channel = new InMemoryChannel();
 IServiceCollection services = new ServiceCollection();
 
-// Being a regular console app, there is no appsettings.json or configuration providers enabled by default.
-// Hence instrumentation key/ connection string and any changes to default logging level must be specified here.
-services.AddLogging(loggingBuilder => loggingBuilder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>("Category", LogLevel.Information));
-services.AddApplicationInsightsTelemetryWorkerService((ApplicationInsightsServiceOptions options) =>
-{
-    options.InstrumentationKey = System.Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
-    Console.WriteLine($"Going to use the instrumentation key {options?.InstrumentationKey}");
-});
+services.Configure<TelemetryConfiguration>(config => config.TelemetryChannel = channel);
 
-// To pass a connection string
-// - aiserviceoptions must be created
-// - set connectionstring on it
-// - pass it to AddApplicationInsightsTelemetryWorkerService()
+services.AddLogging(builder =>
+{
+    // Only Application Insights is registered as a logger provider
+    builder.AddApplicationInsights(
+        configureTelemetryConfiguration: (config) => config.ConnectionString = System.Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"),
+        configureApplicationInsightsLoggerOptions: (options) => { }
+    );
+});
 
 // Build ServiceProvider.
 IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+
 
 // Obtain logger instance from DI.
 ILogger<Program> logger = serviceProvider.GetRequiredService<ILogger<Program>>();
@@ -29,4 +33,6 @@ ILogger<Program> logger = serviceProvider.GetRequiredService<ILogger<Program>>()
 logger.LogInformation("This is an information item from the console logger");
 logger.LogWarning("This is a warning item from the console logger");
 logger.LogError(new DivideByZeroException(), "there was divide by zero error from the console exe");
+channel.Flush();
+Task.Delay(5000).Wait();
 return;
