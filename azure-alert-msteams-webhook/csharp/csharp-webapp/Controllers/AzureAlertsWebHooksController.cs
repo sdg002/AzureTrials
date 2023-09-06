ba0ca7db4a2d5d14bd750d715db5566a049ea655
saurabh <saurabh_dasgupta@hotmail.com>
2023-09-06 08:39:53 +0100

    Added new controller for web hooks

---
+++ azure-alert-msteams-webhook/csharp/csharp-webapp/Controllers/AzureAlertsWebHooksController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AzureAlertsWebHooksController : ControllerBase
+    {
+        private readonly ILogger<AzureAlertsWebHooksController> _logger;
+
+        public AzureAlertsWebHooksController(ILogger<AzureAlertsWebHooksController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        [HttpPost]
+        [Route("CatchIncomingMessage")]
+        public async Task<string> CatchIncomingMessage()
+        {
+            var body = await new StreamReader(this.Request.Body).ReadToEndAsync();
+            _logger.LogInformation("Inside method {method}, {body}", nameof(CatchIncomingMessage), body);
+            return body;
+        }
+
+    }
+
+}
