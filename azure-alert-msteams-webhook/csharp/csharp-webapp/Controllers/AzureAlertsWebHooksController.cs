ed991885b28a89a835db074bec4cce2d8b2c1905
saurabh <saurabh_dasgupta@hotmail.com>
2023-09-06 18:23:47 +0100

    Posting to teams

--- azure-alert-msteams-webhook/csharp/csharp-webapp/Controllers/AzureAlertsWebHooksController.cs
+++ azure-alert-msteams-webhook/csharp/csharp-webapp/Controllers/AzureAlertsWebHooksController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace csharp_webapp.Controllers
 {
@@ -23,7 +26,28 @@ namespace csharp_webapp.Controllers
             _logger.LogInformation("Inside method {method}, {body}", nameof(CatchIncomingMessage), body);
             return body;
         }
+        [HttpGet]
+        [HttpPost]
+        [Route("RelayIncomingMessage")]
+        public async Task<string> RelayIncomingMessage()
+        {
+            var body = await new StreamReader(this.Request.Body).ReadToEndAsync();
+            _logger.LogInformation("Inside method {method}, {body}", nameof(CatchIncomingMessage), body);
+            var mslink = "https://petroineos.webhook.office.com/webhookb2/27a85953-b219-48a9-8381-10515a7df0fd@65795a07-17bb-4e5a-a1fa-1c3d63e0a4fd/IncomingWebhook/7c76e7ba05c24cdc8b301f46772af8ea/61665664-9bff-4859-bd66-55797f42dada";
+            await RelayTextToTeams(mslink,$"hello world {DateTime.Now}");
+            return body;
+        }
 
+        private async Task RelayTextToTeams(string channelEndPoint, string text)
+        {
+            //you were here, call a get use hard coded url
+            //then get the JSON and get url
+            //pass URL from Postman using sample payload
+            var payload = new { text = text };
+            var jsonPayload = JsonSerializer.Serialize(payload);
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync(channelEndPoint, new StringContent(jsonPayload));
+        }
     }
 
 }
