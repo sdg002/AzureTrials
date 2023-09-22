0459113053caa1780896eeab99b7b37ab694f8ce
saurabh <saurabh_dasgupta@hotmail.com>
2023-09-22 23:46:09 +0100

    SecretClient made it to work

--- key-rotation-eventhub/TestProject1/TestProject1/UnitTest1.cs
+++ key-rotation-eventhub/TestProject1/TestProject1/UnitTest1.cs
@@ -1,4 +1,10 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Azure.Identity;
+using Microsoft.Azure.KeyVault;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
 
 namespace TestProject1
 {
@@ -6,7 +12,7 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InMemoryConfiguration()
         {
             //You were here, do some key vault stuff
 
@@ -20,6 +26,73 @@ namespace TestProject1
                     .Build();
 
                 Assert.AreEqual<string>("SomeValue", configuration!["SomeKey"]);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public async Task KeyVault_SecretClient()
+        {
+            try
+            {
+                //
+                //you were reading this https://learn.microsoft.com/en-us/azure/key-vault/secrets/quick-create-net?tabs=azure-cli
+                //you were about to install the package Azure.Security.KeyVault.Secrets
+
+                //var secretClient = new KeyVaultClient(new Defau)
+                string keyVaultName = "saudemovault456";
+                var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+                var client = new Azure.Security.KeyVault.Secrets.SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+
+                var secretName = "eventhubcnstring";
+                var secret=await client.GetSecretAsync(secretName);
+                Trace.WriteLine($"Value of secret:{secretName} is {secret.Value.Value}");
+
+                
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+        [TestMethod]
+        public void KeyVaultConfiguration()
+        {
+            //You were here, do some key vault stuff
+
+            try
+            {
+                //
+                //you were reading this https://learn.microsoft.com/en-us/azure/key-vault/secrets/quick-create-net?tabs=azure-cli
+                //you were about to install the package Azure.Security.KeyVault.Secrets
+
+                var keyVaultConfigOptions = new AzureKeyVaultConfigurationOptions
+                {
+                    ReloadInterval = TimeSpan.FromSeconds(10),
+                    Vault = "saudemovault456"
+                };
+                //var secretClient = new KeyVaultClient(new Defau)
+                string keyVaultName = "saudemovault456";
+                var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+                var client = new Azure.Security.KeyVault.Secrets.SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+                var configuration = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string?>()
+                    {
+                        ["SomeKey"] = "SomeValue"
+                    })
+                    .Build();
+
+                Assert.AreEqual<string>("SomeValue", configuration!["SomeKey"]);
+
+                var eventHub = configuration["eventhubcnstring"];
             }
             catch (Exception ex)
             {
