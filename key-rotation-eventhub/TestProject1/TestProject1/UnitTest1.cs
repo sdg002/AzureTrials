a91a7ab5ad242131c45d8bcf667ba3590839eb80
saurabh <saurabh_dasgupta@hotmail.com>
2023-09-23 18:34:45 +0100

    moved test to BasicConfigurationBuilderDemo

--- key-rotation-eventhub/TestProject1/TestProject1/UnitTest1.cs
+++ key-rotation-eventhub/TestProject1/TestProject1/UnitTest1.cs
@@ -11,27 +11,6 @@ namespace TestProject1
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void InMemoryConfiguration()
-        {
-            //You were here, do some key vault stuff
-
-            try
-            {
-                var configuration = new ConfigurationBuilder()
-                    .AddInMemoryCollection(new Dictionary<string, string?>()
-                    {
-                        ["SomeKey"] = "SomeValue"
-                    })
-                    .Build();
-
-                Assert.AreEqual<string>("SomeValue", configuration!["SomeKey"]);
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
 
         [TestMethod]
         public async Task KeyVault_SecretClient()
@@ -96,6 +75,54 @@ namespace TestProject1
                 var secretName = "eventhubcnstring";
                 var secretValue = configuration[secretName];
                 Trace.WriteLine($"Value of secret:{secretName} is {secretValue}");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        [TestMethod]
+        public void KeyVault_ConfigurationBuilder_Refresh()
+        {
+            //You were here, do some key vault stuff
+
+            try
+            {
+                //
+                //you were reading this https://learn.microsoft.com/en-us/azure/key-vault/secrets/quick-create-net?tabs=azure-cli
+                //you were about to install the package Azure.Security.KeyVault.Secrets
+
+                const int RefreshSeconds = 30;
+                var keyVaultConfigOptions = new Azure.Extensions.AspNetCore.Configuration.Secrets.AzureKeyVaultConfigurationOptions
+                {
+                    ReloadInterval = TimeSpan.FromSeconds(RefreshSeconds),
+                };
+                //var secretClient = new KeyVaultClient(new Defau)
+                string keyVaultName = "saudemovault456";
+                var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+                //var client = new Azure.Security.KeyVault.Secrets.SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+                var configuration = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string?>()
+                    {
+                        ["SomeKey"] = "SomeValue"
+                    })
+                    .AddAzureKeyVault(new Uri(kvUri), new DefaultAzureCredential(), keyVaultConfigOptions)
+                    .Build();
+
+
+                var eventHub = configuration["eventhubcnstring"];
+                var secretName = "eventhubcnstring";
+
+                int counter = 0;
+                while (true) 
+                {
+                    var secretValue = configuration[secretName];
+                    Trace.WriteLine($"{DateTime.Now}:Value of secret:{secretName} is {secretValue}");
+                    Trace.WriteLine($"Waiting....{counter}");
+                    Thread.Sleep(5000);
+                }
             }
             catch (Exception ex)
             {
