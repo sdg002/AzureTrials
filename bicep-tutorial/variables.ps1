e3e5523edb7fabd68a40b3b359bf99799431e7c3
saurabh <saurabh_dasgupta@hotmail.com>
2023-12-07 20:21:32 +0000

    skeletal files

---
+++ bicep-tutorial/variables.ps1
@@ -0,0 +1,16 @@
+Set-StrictMode -Version "latest"
+$ErrorActionPreference="Stop"
+$global:ResourceGroup="RG-BICEP-DEMO-001"
+$Global:Location="uksouth"
+$Global:KeyVault="saudemovault456"
+
+
+<#
+This function should be called after every invocation of Azure CLI to check for success
+#>
+function RaiseCliError($message){
+    if ($LASTEXITCODE -eq 0){
+        return
+    }
+    Write-Error -Message $message
+}
