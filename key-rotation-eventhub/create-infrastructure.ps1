dd1b2400ddffa5baf1470d392a08ef5719e73e79
saurabh <saurabh_dasgupta@hotmail.com>
2023-12-07 20:12:00 +0000

    Feature/eventhub key rotation (#9)
    * Getting primary cn string
    
    * Snippet for key generation
    
    * created key vault via ARM
    
    * created event hub via arm
    
    * Display secrets
    
    * Got event hub connection string into key vault
    
    * Got key vault secret to work with event hub
    
    * Got key rotation to work
    
    * renamed to displayallkeyvaultsecrets
    
    * tidy up rotation and display
    
    * csharp proj
    
    * Got InMemoryCollection config to work
    
    * SecretClient made it to work
    
    * SecretClientOptions
    
    * ConfigurationBuilder approach - completed
    
    * configuration build and key vault - refresh check
    
    * moved test to BasicConfigurationBuilderDemo
    
    * environment config
    
    * renamed to KeyVaultTest1.cs
    
    * Key vault test tidy up
    
    * Updated readme.md
    
    * added event grid pics and references
    
    * Updates to README.MD

---
+++ key-rotation-eventhub/create-infrastructure.ps1
@@ -0,0 +1,41 @@
+. $PSScriptRoot\variables.ps1
+
+<#
+We are querying the key of the storage account using Azure CLI and adding this key as a secret to the Azure KeyVault using the CLI
+#>
+
+
+$armTemplateFile=Join-Path -Path $PSScriptRoot -ChildPath "templates/keyvault.arm.json"
+$armParameterFile=Join-Path -Path $PSScriptRoot -ChildPath "templates/keyvault.parameters.json"
+
+Write-Host "Going to create the Key Vault $VaultName using ARM template $armTemplateFile"
+& az deployment group create --resource-group $Global:ResourceGroup --template-file $armTemplateFile `
+    --parameters  `
+    @$armParameterFile `
+    name=$Global:KeyVault `
+    --verbose
+
+RaiseCliError -message "Failed to create the Key Vault"
+
+
+$armTemplateFile=Join-Path -Path $PSScriptRoot -ChildPath "templates/eventhub.arm.json"
+
+Write-Host "Going to create the Event Hub  $Global:EventHubNameSpace using ARM template $armTemplateFile"
+& az deployment group create --resource-group $Global:ResourceGroup --template-file $armTemplateFile `
+    --parameters  `
+    name=$Global:EventHubNameSpace `
+    --verbose
+
+RaiseCliError -message "Failed to create the Key Vault"
+
+
+$armTemplateFile=Join-Path -Path $PSScriptRoot -ChildPath "templates/keyvault-secrets.arm.json"
+
+Write-Host "Going to add secrets to the Key Vault $Global:KeyVault using ARM template $armTemplateFile"
+& az deployment group create --resource-group $Global:ResourceGroup --template-file $armTemplateFile `
+    --parameters  `
+    keyVaultName=$Global:KeyVault `
+    eventHubName=$Global:EventHubNameSpace `
+    --verbose
+
+RaiseCliError -message "Failed to add secrets to the Key Vault"
