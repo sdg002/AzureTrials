5320b59eecc27ced686553739672e6e74d661894
saurabh <saurabh_dasgupta@hotmail.com>
2023-12-31 20:00:42 +0000

    VNET applied to func

--- bicep-tutorial/108-firewall-settings/deploy.ps1
+++ bicep-tutorial/108-firewall-settings/deploy.ps1
@@ -27,3 +27,18 @@ Write-Host "Deploying VNET $Global:Vnet"
     --parameters name=$Global:Vnet  --verbose
 RaiseCliError -message "Failed to deploy VNET $Global:Vnet"
 
+
+<#
+Deploying function app with VNET for outbound
+#>
+Write-Host "Deploying  Function App $Global:FirstFunctionApp"
+& az deployment group create --resource-group $Global:ResourceGroup `
+    --template-file "$PSScriptRoot\templates\functionapp.bicep" `
+    --parameters name=$Global:FirstFunctionApp `
+    storageaccount=$Global:FunctionStorageAccount `
+    planname=$Global:AppPlan `
+    appinsight=$Global:ApplicationInsights `
+    vnet=$Global:Vnet `
+    subnet=$Global:Subnet  `
+    --verbose
+RaiseCliError -message "Failed to create function app $Global:FirstFunctionApp"
