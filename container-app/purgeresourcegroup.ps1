a238bfb6749f5046d72b2a66b71e7916941ab377
saurabh <saurabh_dasgupta@hotmail.com>
2024-06-25 18:26:02 +0100

    Got purge to work

--- container-app/purgeresourcegroup.ps1
+++ container-app/purgeresourcegroup.ps1
@@ -1,5 +1,41 @@
-$Global:ResourceGroup="rg-demo-container-apps-dev-uks"
+. $PSScriptRoot/common.ps1
+
+
 
 $Global:ResourceGroup
 
-az resource list --resource-group $Global:ResourceGroup
+$resources=(az resource list --resource-group $Global:ResourceGroup | ConvertFrom-Json -AsHashTable)
+
+Write-host ("Found {0} resources" -f $resources.Count)
+
+
+
+function DeleteResources()
+{
+    $count_deleted=0
+    foreach($resource in $resources) 
+    { 
+    
+        try {
+            $id=$resource["id"] 
+            Write-Host "Going to delete resource with id: $id"
+            & az resource delete --ids $id --verbose
+            $count_deleted+=1
+        }
+        catch {
+            Write-Host $_
+        }
+    }
+    return $count_deleted
+}
+
+while ($true) 
+{
+    $deleted=DeleteResources
+    if (0 -eq $deleted)
+    {
+        Write-Host "No more resources to delete"
+        break;
+    }
+    Write-Host "No of resources deleted=$deleted Going to try again"
+}
