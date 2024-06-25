18b0f3a4424b0369f98d4fcb78d78c8e813a63c8
saurabh <saurabh_dasgupta@hotmail.com>
2024-06-25 18:01:19 +0100

    Created comon.ps1

--- container-app/100/commonvariables.ps1
+++ container-app/100/commonvariables.ps1
@@ -1,26 +1,9 @@
-Set-StrictMode -Version "latest"
-$ErrorActionPreference="Stop"
+. $PSScriptRoot/../common.ps1
 
 
 
-$Global:environment=$env:ENVIRONMENT
-if ([string]::IsNullOrWhiteSpace($Global:environment)){
-    Write-Error -Message "The environment variable 'ENVIRONMENT' was not set"
-}
-Write-Host "The environment is $Global:environment"
-$Global:ResourceGroup="rg-demo-container-apps-$Global:environment-uks"
-$Global:Location="uksouth"
 $Global:LogAnalytics="democontainerapplogworkspace$($Global:environment)"
 $Global:ContainerAppsEnvironment="caedemosau$($Location)001"
 $Global:ContainerRegistry=("saupycontainerregistry001{0}" -f $env:environment)
 $Global:ContainerApp001=("casau{0}{1}" -f $env:environment, $Global:Location)
 
-<#
-This function should be called after every invocation of Azure CLI to check for success
-#>
-function RaiseCliError($message){
-    if ($LASTEXITCODE -eq 0){
-        return
-    }
-    Write-Error -Message $message
-}
---
+++ container-app/purgeresourcegroup.ps1
@@ -0,0 +1,5 @@
+$Global:ResourceGroup="rg-demo-container-apps-dev-uks"
+
+$Global:ResourceGroup
+
+az resource list --resource-group $Global:ResourceGroup
