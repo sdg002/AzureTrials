f9240bbab36b1388b63d731905dbf92bf299cfe5
saurabh <saurabh_dasgupta@hotmail.com>
2024-06-26 08:04:20 +0100

    Container app 100 (#14)
    * Initial file
    
    * Created resource group
    
    * log analytics
    
    * deployed container environment
    
    * container registry
    
    * Deployed container app
    
    * nginx
    
    * Created comon.ps1
    
    * Got purge to work
    
    * Got purge to work

---
+++ container-app/100/deploy.ps1
@@ -0,0 +1,48 @@
+. $PSScriptRoot/commonvariables.ps1
+
+<#
+Deploy resource group
+#>
+Write-Host "Going to create the resource group: '$Global:ResourceGroup' with the location: '$Global:Location' "
+& az group create --location $Global:Location --name $Global:ResourceGroup `
+    --tags  location=$Global:Location "department='my department'"
+RaiseCliError -message "Failed to create the resource group $Global:ResourceGroup"
+
+<#
+Create log analytics workspace
+#>
+Write-Host "Deploying log analytics $Global:LogAnalytics"
+& az deployment group create --resource-group $Global:ResourceGroup `
+    --template-file "$PSScriptRoot\bicep\loganalytics.bicep" `
+    --parameters name=$Global:LogAnalytics  --verbose
+RaiseCliError -message "Failed to create log analytics workspace $Global:LogAnalytics"
+
+<#
+Create Azure Container Environment
+#>
+Write-Host "Deploying container apps environment $Global:ContainerAppsEnvironment"
+& az deployment group create --resource-group $Global:ResourceGroup `
+    --template-file "$PSScriptRoot\bicep\acaenvironment.bicep" `
+    --parameters name=$Global:ContainerAppsEnvironment  `
+    logworkspacename=$Global:LogAnalytics `
+    --verbose
+RaiseCliError -message "Failed to create container apps environment $Global:ContainerAppsEnvironment"
+
+<#
+Create Azure Container Registry
+#>
+Write-Host "Creating container registry $ContainerRegistry"
+& az acr create --resource-group $Global:ResourceGroup --name $ContainerRegistry --sku Basic --admin-enabled true
+RaiseCliError -message "Failed to create container apps environment $Global:ContainerAppsEnvironment"
+
+<#
+Create Container App 
+#>
+Write-Host "Creating container app $Global:ContainerApp001"
+& az deployment group create --resource-group $Global:ResourceGroup `
+    --template-file "$PSScriptRoot\bicep\app001.bicep" `
+    --parameters `
+    acaenvironmentname=$Global:ContainerAppsEnvironment  `
+    name=$Global:ContainerApp001 `
+    --verbose
+RaiseCliError -message "Failed to create container app  $Global:ContainerApp001"
---
+++ container-app/common.ps1
@@ -0,0 +1,18 @@
+Set-StrictMode -Version "latest"
+$ErrorActionPreference="Stop"
+$Global:environment=$env:ENVIRONMENT
+if ([string]::IsNullOrWhiteSpace($Global:environment)){
+    Write-Error -Message "The environment variable 'ENVIRONMENT' was not set"
+}
+Write-Host "The environment is $Global:environment"
+$Global:ResourceGroup="rg-demo-container-apps-$Global:environment-uks"
+$Global:Location="uksouth"
+<#
+This function should be called after every invocation of Azure CLI to check for success
+#>
+function RaiseCliError($message){
+    if ($LASTEXITCODE -eq 0){
+        return
+    }
+    Write-Error -Message $message
+}
