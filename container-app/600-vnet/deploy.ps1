a84b8c4cb3556992536073441388f2160a65f880
saurabh <saurabh_dasgupta@hotmail.com>
2024-08-08 17:18:09 +0100

    deploy container environment with vnet

--- container-app/600-vnet/deploy.ps1
+++ container-app/600-vnet/deploy.ps1
@@ -13,3 +13,17 @@ Write-Host "Deploying VNET"
 RaiseCliError -message "Failed to create VNET"
 
 
+<#
+Create Azure Container Environment
+#>
+Write-Host "Deploying container apps environment $Global:ContainerAppsEnvironment"
+& az deployment group create --resource-group $Global:ResourceGroup `
+    --template-file "$PSScriptRoot\bicep\acaenvironment.bicep" `
+    --parameters name=$Global:ContainerAppsEnvironment  `
+    logworkspacename=$Global:LogAnalytics `
+    identityname=$Global:ContainerAppsEnvironmentManagedIdentity `
+    vnetname=$Global:Vnet `
+    --verbose
+RaiseCliError -message "Failed to create container apps environment $Global:ContainerAppsEnvironment"
+
+
