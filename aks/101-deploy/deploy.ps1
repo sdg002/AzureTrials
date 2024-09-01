f4ee5fdc8923da677e3ecea95f144aaf37f526ca
saurabh <saurabh_dasgupta@hotmail.com>
2024-09-01 14:56:13 +0100

    Created resource group

---
+++ aks/101-deploy/deploy.ps1
@@ -0,0 +1,8 @@
+. $PSScriptRoot/../common.ps1
+
+<#
+Deploy resource group
+#>
+Write-Host "Going to create the resource group: '$Global:ResourceGroup' with the location: '$Global:Location' "
+& az group create --location $Global:Location --name $Global:ResourceGroup `
+    --tags $Global:TagsArray
