1d6bf5d88ccacb8d26fdc0ed8a62eba4321102ae
saurabh <saurabh_dasgupta@hotmail.com>
2024-09-03 11:06:05 +0100

    Got flask app to run locally

--- aks/demo-flask-app/src/app.py
+++ aks/demo-flask-app/src/app.py
@@ -1,1 +1,15 @@
-print("hello world")
+"""
+This is a hello world Web application
+"""
+import datetime as dt
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    now=dt.datetime.now()
+    return f"Hello world , current date is {now}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
