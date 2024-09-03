bb050228cb201a91313ed2e21a532f148b9d8867
saurabh <saurabh_dasgupta@hotmail.com>
2024-09-03 13:29:16 +0100

    Deployed service

--- aks/300-deploy-web-app/aksdeploy.ps1
+++ aks/300-deploy-web-app/aksdeploy.ps1
@@ -3,3 +3,7 @@
 Write-Host "Going to create deployment"
 kubectl apply --filename "$PSScriptRoot/aks-deployment.yaml" --namespace demoapp
 RaiseCliError -message "Failed to create deployment"
+
+Write-Host "Going to create service"
+kubectl apply --filename "$PSScriptRoot/aks-service.yaml" --namespace demoapp
+RaiseCliError -message "Failed to create service"
