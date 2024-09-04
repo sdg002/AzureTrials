a88b59117485bd75bf32bec9a7b0f578fd0d3ce2
saurabh <saurabh_dasgupta@hotmail.com>
2024-09-04 16:01:31 +0100

    Got the job deployment to work

--- aks/400-deploy-demo-job/aksdeploy.ps1
+++ aks/400-deploy-demo-job/aksdeploy.ps1
@@ -1,10 +1,5 @@
 . $PSScriptRoot/../common.ps1
 
-Write-Host "Going to create pod"
-kubectl apply --filename $PSScriptRoot/pod.yaml --namespace=demoapp
-RaiseCliError -message "Failed to create Pod"
-Write-Host "Created the pod"
-
 Write-Host "Going to create deployment"
 kubectl apply --filename "$PSScriptRoot/aks-deployment.yaml" --namespace demoapp
 RaiseCliError -message "Failed to create deployment"
--- aks/demo-job/src/main.py
+++ aks/demo-job/src/main.py
@@ -1,9 +1,28 @@
+"""
+Long running job
+"""
 from time import sleep
+import os
+APP_VERSION=1.6
 
 COUNT=10
 
+def display_variables()->str:
+    keys=list(os.environ.keys())
+    keys.sort()
+    print("----------------------")
+    result=""
+    print(f"Version:{APP_VERSION}<br/>")
+    print("----------------<br/>")
+    for key in keys:
+        line=(f"{key}={os.environ[key]}<br/>")
+        print(line)
+    print("----------------------")
+    return result
+
 if __name__ == "__main__":
     print("Begin")
+    display_variables()
     for idx in range(COUNT):
         print(f"This is attempt number {idx}\n")
         print("--------------------------\n")
